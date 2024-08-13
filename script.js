abfbe6ac7f5e4906676e65dff40ce11dbc6f6964
bellysilvah <143099258+bellysilvah@users.noreply.github.com>
2024-08-13 16:39:40 -0300

    criamoas pergunta

---
+++ script.js
@@ -0,0 +1,23 @@
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const tetoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+    {
+        enunciado: "pergunta 1",
+        alternativa:[
+            "alternativa 1",
+            "alternativa 2"
+        ]
+    },
+    {
+       enunciado: "pergunta 2" ,
+       alternativa:[
+        "alternativa 1",
+        "alternativa 2"
+       ]
+    }
+];
+]
