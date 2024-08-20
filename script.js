f9201cceb399dc5f7e3ff77754c3c68e06fe65ec
bellysilvah <143099258+bellysilvah@users.noreply.github.com>
2024-08-20 16:24:53 -0300

    criação da escola

--- script.js
+++ script.js
@@ -5,19 +5,47 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const tetoResultado = document.querySelector(".texto-resultado");
 
 const perguntas = [
-    {
-        enunciado: "pergunta 1",
+      {
+         enunciado: " Assim que saiu da escola Gabriel se depara com uma nova tecnologia, um chat que consegue responder todas as dúvidas que uma pessoa pode ter, o chat também gera imagens e áudios hiper-realistas. Qual o primeiro pensamento de Gabriel?",
         alternativa:[
-            "alternativa 1",
-            "alternativa 2"
+            "Isso é assustador!",
+            "Isso é maravilhoso!"
         ]
     },
     {
-       enunciado: "pergunta 2" ,
+       enunciado: "Com a descoberta desta tecnologia uma professora de tecnologia da escola decidiu fazer uma sequência de aulas sobre IA. No fim de uma aula ela pede que Gabriel escreva um trabalho sobre o uso de tecnologia em sala de aula. Qual atitude Gabriel toma?",
        alternativa:[
-        "alternativa 1",
-        "alternativa 2"
+        "Utiliza uma ferramenta de busca na internet que utiliza IA para que ela ajude a encontrar informações relevantes para o trabalho e explique numa linguagem que facilite o entendimento.",
+        "Escreve o trabalho com base nas conversas que teve com colegas, algumas pesquisas na internet e conhecimentos próprios sobre o tema."
        ]
-    }
+    },
+    {
+        enunciado: "pergunta 1",
+       alternativa:[
+           "alternativa 1",
+           "alternativa 2"
+       ]
+   },
+   {
+    enunciado: "pergunta 1",
+   alternativa:[
+       "alternativa 1",
+       "alternativa 2"
+   ]
+},
+{
+    enunciado: "pergunta 1",
+   alternativa:[
+       "alternativa 1",
+       "alternativa 2"
+   ]
+},
 ];
-]
+
+let atual = 0; 
+let perguntaAtual;
+
+function mostraPergunta(){
+    perguntaAtual =perguntas[atual];
+    caixaPerguntas.textContent = perguntaAtual.enunciado;
+}
